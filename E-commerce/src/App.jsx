d566dfad75102c3a1f4c23c18ce6f6c28bceddc4
Nasir-afridi <nasirwc05@gmail.com>
2025-06-27 11:58:36 +0500

    added the free delievery category

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Body from './components/Body'
+import { Category } from "./components/Category";
 
 function App() {
   return (
     <>
       <Navbar />
       <Body />
+      <Category></Category>
     </>
   );
 }
---
+++ E-commerce/src/components/Category.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Category = () => {
+  return (
+    <>
+      <div class="container py-5 mt-5">
+        <div class="row text-center text-md-start">
+          <div class="col-12 col-sm-6 col-md-3 mb-4">
+            <div class="d-flex align-items-start gap-3 justify-content-center justify-content-md-start">
+              <i class="bi bi-cart fs-3 text-danger"></i>
+              <div>
+                <h6 class="fw-bold mb-1">Free Delivery</h6>
+                <p class="mb-0 text-muted">
+                  Consectetur adipi elit lorem ipsum dolor sit amet.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
