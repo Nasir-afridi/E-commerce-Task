ea93ad7825aeb17915df089709f28b8d7ac74659
Nasir-afridi <nasirwc05@gmail.com>
2025-06-27 12:05:07 +0500

    added the remaining three category

--- E-commerce/src/components/Category.jsx
+++ E-commerce/src/components/Category.jsx
@@ -16,6 +16,37 @@ export const Category = () => {
               </div>
             </div>
           </div>
+
+    <div class="col-12 col-sm-6 col-md-3 mb-4">
+      <div class="d-flex align-items-start gap-3 justify-content-center justify-content-md-start">
+        <i class="bi bi-award fs-3 text-danger"></i>
+        <div>
+          <h6 class="fw-bold mb-1">Quality Guarantee</h6>
+          <p class="mb-0 text-muted">Dolor sit amet orem ipsu mcons ectetur adipi elit.</p>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="col-12 col-sm-6 col-md-3 mb-4">
+      <div class="d-flex align-items-start gap-3 justify-content-center justify-content-md-start">
+        <i class="bi bi-tag fs-3 text-danger"></i>
+        <div>
+          <h6 class="fw-bold mb-1">Daily Offers</h6>
+          <p class="mb-0 text-muted">Amet consectetur adipi elit lorem ipsum dolor sit.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-sm-6 col-md-3 mb-4">
+      <div class="d-flex align-items-start gap-3 justify-content-center justify-content-md-start">
+        <i class="bi bi-shield-plus fs-3 text-danger"></i>
+        <div>
+          <h6 class="fw-bold mb-1">100% Secure Payment</h6>
+          <p class="mb-0 text-muted">Rem Lopsum dolor sit amet, consectetur adipi elit.</p>
+        </div>
+      </div>
+    </div>
         </div>
       </div>
     </>
